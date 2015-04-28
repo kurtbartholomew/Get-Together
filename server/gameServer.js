a33cc441d5ca0156e59405a79dd8b932ba015f02
Kurt Bartholomew <siever246@gmail.com>
2015-04-27 21:05:42 -0700

    Added additional socket logic for timeouts, players leaving, and general update actions sent from client

--- server/gameServer.js
+++ server/gameServer.js
@@ -2,15 +2,15 @@ var server = function(ioServer) {
 
   var timeOutDelay = 2500;
   var maxPlayers = 4;
-  var runningGameIds = [];
-  var clientPlayers = {};
+  var runningGameIds = []; // id's of currently running games
+  var clientPlayers = {}; // keeps track of which number out of four a player is
   var clients = {}; // stores a socket id with a value of the room they are in
   var hosts = {}; // stores a socket id with a value of true to represent their host status
 
 
   var contains = function(element, array) {
     return array.indexOf(element) > -1;
-  }
+  };
 
   /**
    * Creates a unique 4 character string of a radix 36 representation
@@ -23,7 +23,7 @@ var server = function(ioServer) {
       randomNum = (Math.random()*900000000).toString(36).substr(0,4);
     }
     return randomNum;
-  }
+  };
 
   /**
    * Checks if a room exists in the root namespace
@@ -68,6 +68,36 @@ var server = function(ioServer) {
     }
   };
 
+  var sendError = function(message, socket, room) {
+    if (room !== undefined) {
+      socket = socket.to(room);
+    }
+    socket.emit('errorMsg', { msg: msg });
+    console.log("Error: ", msg);
+  }
+
+  var startTimeOut = function(room, playerOrdinal, timeoutCounter) {
+    playerOrdinal = playerOrdinal || 0;
+    timeoutCounter = timeoutCounter || 0;
+
+    var currentPlayers = socketsInRoom(room);
+
+    if (timeoutCounter > 3) {
+      return;
+    } else {
+      var socketId = currentPlayers[playerOrdinal];
+      var socketObj = io.sockets.connected[currentPlayers[playerOrdinal]];
+      if (socket !== undefined) {
+        socket.emit('timeOut', {times: times}, function(socketId){
+          startTimeOut(room, playerOrdinal++, timeoutCounter);
+        });
+      } else {
+        console.log("Error finding socket during timeout check: ",socketId);
+      }
+    }
+  }
+
+
   ioServer.on('connection', function(socket){
     clients[socket.id] = null;
     
@@ -90,28 +120,76 @@ var server = function(ioServer) {
     socket.on('join', function(data,ack){
       var room = data;
       if(roomExists(room)) {
-        socket.join(room, function (err) {
-          if (!err) {
-            clients[socket.id] = room;
-            var players = socketsInRoom(room);
-            clientPlayers[socket.id] = players.length -1;
-            ack({ playersCount: players.length});
-            io.to(room).emit('joined', { playersCount: players.length });
-          } // write an else to log error and explain client can't join room
-        });
-      } // write an else to explain to client room doesn't exist;
+        var numOfPlayers = socketsInRoom(room).length;
+        if ( numOfPlayers > 0 && numOfPlayers <= maxPlayers){
+          socket.join(room, function (err) {
+            if (!err) {
+              clients[socket.id] = room;
+              var players = socketsInRoom(room);
+              clientPlayers[socket.id] = players.length -1;
+              ack({ playersCount: players.length});
+              io.to(room).emit('joined', { playersCount: players.length });
+            } else {
+              console.log("Error occurred: ",err)
+              sendError("Client: Error joining room", socket);
+            }
+          });
+        } else {
+          sendError("That room is inaccessible", socket);
+        }
+      } else {
+        sendError("That room does not exist", socket);
+      } 
     });
 
-    socket.on('gameUpdate', function(data) {
-      var room = clients[data.socketId];
-      delete data.socketId;
-      io.to(room).emit('clientUpdate', data);
+
+
+    socket.on('beginCheckingForTimeout', function(socketId) {
+      var room = clients[socketId];
+      var players = socketsInRoom(room);
+
+      if (players.length == maxPlayers) {
+        setTimeout(function() {
+          startTimeOut(room);
+        }, timeOutDelay);
+      } else {
+        sendError("Other players have timed out: ", socket, room); 
+      }
     });
 
+    socket.on('disconnect', function() {
+      var p = clientPlayers[socket.id];
+      clientPlayers[socket.id] = null;
+      delete clientPlayers[socket.id];
 
-    
+      var room = clients[socket.id];
+      clients[socket.id] = null;
+      delete clients[socket.id];
 
+      var currentPlayers = socketsInRoom(room);
 
+      if (room != null && currentPlayers.length > 0) {
+        io.to(room).emit('playerLeft', { playerLeft: p, playersCount: currentPlayers.length});
+        
+        // if disconnecting player is host and there are players left...
+        if(hosts[socket.id] && currentPlayers.length > 1) {
+          hosts[socket.id] = false; 
+          delete hosts[socket.id];
+
+          var newSocketId = currentPlayers[Math.floor(Math.random() * players.length)];
+          hosts[newSocketId] = true;
+
+          getSocket(newSocketId).emit('makeHost');
+        } else if ( currentPlayers.length == 1 ) {
+          sendError("Sorry, you can't play with yourself", socket, room);
+        }
+      } else {
+        if( games[room] !== undefined) {
+          delete games[room];
+        }
+        console.log("Room "+room+" has been removed");
+      }
+    });
 
 
     console.log("A player has connected");
@@ -120,9 +198,25 @@ var server = function(ioServer) {
       console.log("Name: ", data.user);
       socket.emit('pong', { greeting: 'HELLO!' });
     });
-    
+
+    socket.on('gameUpdate', function(data) {
+      var room = clients[data.socketId];
+      delete data.socketId;
+      io.to(room).emit('clientUpdate', data);
+    });
+
+    socket.on('gameScoreUpdate', function(data) {
+      var room = clients[data.socketId];
+      delete data.socketId;
+      io.to(room).emit('clientUpdateScores', data);
+    });
+
+    socket.on('gameBallUpdate', function(data) {
+      var room = clients[data.socketId];
+      delete data.socketId;
+      io.to(room).emit('clientUpdateBall', data);
+    });
   });
-  
 };
 
 module.exports = server;
